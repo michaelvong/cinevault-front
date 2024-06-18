1b5b23bbe34a5616c4c11427d017c18f35801528
michaelvong <mvong006@ucr.edu>
2024-06-18 16:50:50 -0700

    added new arrivals to be displayed

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import NewMovies from "@/components/NewMovies";
 import { mongooseConnect } from "@/lib/mongoose";
 import {Product} from "@/models/Product";
 
-export default function Home({product}){
+export default function Home({featuredProduct, newProducts}){
   return (
     <div>
       <Header />
-      <Featured product = {product}/>
-      <NewMovies></NewMovies>
+      <Featured product = {featuredProduct}/>
+      <NewMovies products = {newProducts}></NewMovies>
     </div>
     
   );
@@ -18,8 +18,12 @@ export default function Home({product}){
 export async function getServerSideProps(){
   const featuredProductId = '666b7a4c0a2c8e3f794864dd';
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id': -1}, limit: 10})
   return {
-    props: {product: JSON.parse(JSON.stringify(product))},
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
