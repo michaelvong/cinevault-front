cffa3c8dd3101cf953d4df08bc8c156b1de3f44c
michaelvong <mvong006@ucr.edu>
2024-06-19 19:52:31 -0700

    added timestamps to model

--- models/Order.js
+++ models/Order.js
@@ -9,6 +9,8 @@ const OrderSchema = new Schema({
     postalCode: String,
     country: String,
     paid: Boolean,
+}, {
+    timestamps: true,
 });
 
 export const Order = models?.Order || model('Order', OrderSchema);
