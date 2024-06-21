dfd2b69adad0ca1dc832498338bf1ba0780e5afd
michaelvong <mvong006@ucr.edu>
2024-06-20 22:27:32 -0700

    created whitebox component

---
+++ components/WhiteBox.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const WhiteBox = styled.div`
+    background-color: #D6D6D6;
+    border-radius: 10px;
+    padding: 30px;
+    text-align: center;
+`
+
+export default WhiteBox;
