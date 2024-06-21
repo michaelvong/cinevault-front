f3cfc80f980b0bcf7f51950741f96ad44e987373
michaelvong <mvong006@ucr.edu>
2024-06-20 22:28:41 -0700

    changed featured product id and changed limit to 5

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,10 @@ export default function Home({featuredProduct, newProducts}){
 };
 
 export async function getServerSideProps(){
-  const featuredProductId = '666b7a4c0a2c8e3f794864dd';
+  const featuredProductId = '667503628a0b9c97add34f75';
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
-  const newProducts = await Product.find({}, null, {sort: {'_id': -1}, limit: 10})
+  const newProducts = await Product.find({}, null, {sort: {'_id': -1}, limit: 5})
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
