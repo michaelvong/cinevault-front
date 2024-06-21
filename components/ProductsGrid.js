1b3b17eb19562e1bc457f72dba277051a9898e8c
michaelvong <mvong006@ucr.edu>
2024-06-20 20:40:52 -0700

    created a component to display products in a grid

---
+++ components/ProductsGrid.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import ProductBox from "./ProductBox";
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 20px;
+`
+
+export default function ProductsGrid({products}){
+    return (
+        <Grid>
+            {products?.length > 0 && products.map((prod, index) => (
+                <ProductBox {...prod} key={index}/>
+            ))}
+        </Grid>
+    );
+};
